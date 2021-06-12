fe49b6630f25e1798663bb44a400131eda51c36c
gunners6518 <gunners6518@gmail.com>
2021-06-12 11:26:54 +0900

    HOME画面の作成

---
+++ src/views/settings.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+  <h1>Settingだよ</h1>
+</div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+
+  export default Vue.extend({
+    name: 'Setting',
+  })
+</script>
